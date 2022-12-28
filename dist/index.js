90176945b3546e019890fb091f489a05ffe78d19
Kishore <kishore.iiitn@gmail.com>
2022-12-28 13:24:51 +0530

    feat: bundle.

--- dist/index.js
+++ dist/index.js
@@ -9686,16 +9686,47 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
+const fetch = __nccwpck_require__(467);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  (async () => {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+
+    const githubRepoName = github.context.payload.repository; // ?? "sample-nextjs-repo";
+    const githubCommitSha = github.context.payload.after; // ??"3569155c4430cd6b6c1d612d6dc57a302f3fae31";
+
+    const data = await fetch("https://api.vercel.com/v6/deployments", {
+      headers: {
+        Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
+      },
+      method: "get",
+    }).then((v) => v.json());
+
+    while (true) {
+      const deployments = data.deployments;
+      const result = deployments.find(
+        (deployment) =>
+          deployment.meta.githubCommitRepo === githubRepoName &&
+          deployment.meta.githubCommitSha === githubCommitSha
+      );
+
+      if (result) {
+        console.log("DATA: ", JSON.stringify(result, null, "  "));
+        break;
+      } else {
+        console.log("DEPLOYMENTS: " + JSON.stringify(deployments, null, "  "));
+        break;
+      }
+      await new Promise((r) => setTimeout(r, 10000));
+    }
+  })();
 } catch (error) {
   core.setFailed(error.message);
 }
